054652574f24fd5af609cf80b95c9af00a081719
ghazal faris <gazalfaris2005@yahoo.com>
2022-01-12 10:21:58 -0500

    Commit: adds jl files

---
+++ src/Subplots.jl
@@ -0,0 +1,57 @@
+#=Makie uses a powerful layout system under the hood, which allows you to create very complex figures with many subplots. For the easiest way to do this, we need a Figure object. So far, we haven't seen this explicitly, it was created in the background in the first plotting function call.
+
+We can also create a Figure directly and then continue working with it. We can make subplots by giving the location of the subplot in our layout grid as the first argument to our plotting function. The basic syntax for specifying the location in a figure is fig[row, col].
+=#
+
+using CairoMakie
+
+x = LinRange(0, 10, 100)
+y = sin.(x)
+
+fig = Figure()
+lines(fig[1, 1], x, y, color = :red)
+lines(fig[1, 2], x, y, color = :blue)
+lines(fig[2, 1:2], x, y, color = :green)
+
+fig
+#---Each lines call creates a new axis in the position given as the first argument, that's why we use lines and not lines! here.
+
+#=
+Constructing axes manually
+Like Figures, we can also create axes manually. This is useful if we want to prepare an empty axis to then plot into it later.
+
+The default 2D axis that we have created implicitly so far is called Axis and can also be created in a specific position in the figure by passing that position as the first argument.
+
+For example, we can create a figure with three axes.
+=#
+
+using CairoMakie
+
+fig = Figure()
+ax1 = Axis(fig[1, 1])
+ax2 = Axis(fig[1, 2])
+ax3 = Axis(fig[2, 1:2])
+fig
+
+And then we can continue to plot into these empty axes.
+
+lines!(ax1, 0..10, sin)
+lines!(ax2, 0..10, cos)
+lines!(ax3, 0..10, sqrt)
+fig
+
+#=
+Note, the notation 0..10 above creates a closed interval from 0 to 10 (see IntervalSets.jl for further details).
+
+Axes also have many attributes that you can set, for example to give them a title, or labels.
+=#
+
+
+ax1.title = "sin"
+ax2.title = "cos"
+ax3.title = "sqrt"
+
+ax1.ylabel = "amplitude"
+ax3.ylabel = "amplitude"
+ax3.xlabel = "time"
+fig
---
+++ src/simplelegend.jl
@@ -0,0 +1,14 @@
+"""
+If you add label attributes to your plots, you can call the axislegend function to add a legend with all labeled plots to the current axis.
+"""
+
+using CairoMakie
+
+x = range(0, 10, length=100)
+y1 = sin.(x)
+y2 = cos.(x)
+
+lines(x, y1, color = :red, label = "sin")
+lines!(x, y2, color = :blue, label = "cos")
+axislegend()
+current_figure()
